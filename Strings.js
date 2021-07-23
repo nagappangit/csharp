add608ca5968dfeff8a47f26548c1c287ba63745
nagappangit <nagarajeshbe@gmail.com>
2021-07-23 15:55:54 +0530

    commit

--- Strings.js
+++ Strings.js
@@ -1,4 +1,6 @@
 <script>
     let FirstName  = "Nagappan"; // Double Quotes String
+    let LastName = 'Kumaresan' // Single Quotes String
     console.log(FirstName);
+    console.log(LastName);
 </script>
