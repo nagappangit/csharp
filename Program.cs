c51f90bd8abaf19ee49bc822dca42d093591be93
nagappangit <nagarajeshbe@gmail.com>
2021-12-02 19:40:36 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Java_Scripts_Works
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
