65e800a8cb51a0cdde4b07c996837ed43053c584
nagappangit <nagarajeshbe@gmail.com>
2021-12-02 19:50:45 +0530

    cmmit

--- csharp/csharpmethods.cs
+++ csharp/csharpmethods.cs
@@ -1,17 +1,15 @@
 //C# Methods
 using System;
 namespace MyApplication
-{
-  class Program
+{  class csharpmethods
   {
     static void MyMethod()
     {
       int a,b;
       a = 5;
       b = 2;
-      Console.WriteLine(Math.Pow(a, b));
-    }
-    static void Main(string[] args)
+      Console.WriteLine(Math.Pow(a, b)); // math power
+          }    static void Main(string[] args)
     {
       MyMethod();
     }
