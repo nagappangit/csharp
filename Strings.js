1a31a2df2467381f16f75ef94ef440b3503fbb46
nagappangit <nagarajeshbe@gmail.com>
2021-12-02 18:54:46 +0530

    commit

--- Strings.js
+++
@@ -1,6 +0,0 @@
-<script>
-    let FirstName  = "Nagappan"; // Double Quotes String
-    let LastName = 'Kumaresan' // Single Quotes String
-    console.log(FirstName);
-    console.log(LastName);
-</script>
