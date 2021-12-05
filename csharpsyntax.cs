1aa1849087ff4024b4aa88511628bd1788b5d1ad
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2021-12-05 21:35:10 +0530

    Add files via upload

---
+++ csharpsyntax.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MyApplication
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Indoa");
+        }
+    }
+    
+}
