23fc197918fcbe46822eeb308c0796ef12c80d88
nagappangit <nagarajeshbe@gmail.com>
2021-12-06 19:18:29 +0530

    push

---
+++ csharpsyntax.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MyApplication
+{
+    class MyFirstclass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Indoa");
+        }
+    }
+    
+}
