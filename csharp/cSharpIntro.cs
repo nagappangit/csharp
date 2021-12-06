f4156ca2f788a5c923680072d5585c14fa2e6c65
nagappangit <nagarajeshbe@gmail.com>
2021-12-06 17:44:23 +0530

    push

---
+++ csharp/cSharpIntro.cs
@@ -0,0 +1,18 @@
+//C#.NET Tutorial 
+//csharp introduction
+//dont have any full form for .net, this is simply we called as a software product. it's come from microsoft
+//this we used for developing various kinds of application
+//aplications means 1. Desktop Applications 2. Web Applications 3. Mobile Applications we can used .Net
+//.NET have setof programming languages, officialy .NET have 30+ languages
+//CSharp Language is the extension of C++
+using System;
+namespace cSharpIntro
+{
+    class CSharpIntro
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
