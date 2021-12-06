6aee11f61a2788f5148641e1e0aca8f57259548d
nagappangit <nagarajeshbe@gmail.com>
2021-12-06 19:27:22 +0530

    push

--- cSharpIntro.cs
+++ cSharpIntro.cs
@@ -1,4 +1,3 @@
-using Internal;
 //C#.NET Tutorial 
 //csharp introduction
 //dont have any full form for .net, this is simply we called as a software product. it's come from microsoft
@@ -6,7 +5,8 @@ using Internal;
 //aplications means 1. Desktop Applications 2. Web Applications 3. Mobile Applications we can used .Net
 //.NET have setof programming languages, officialy .NET have 30+ languages like C#.NET, VB.NET, VCPP.NET, F#.NET, J#.NET
 //CSharp Language is the extension of C++ means C --> C++ --> C# have an extra features
-//C# Features 1. Its Object Oriented (Security and Reusability) 2. Platfor Independent (That Means Cross Platform) 3. Language Independent (That Means Cross Language Reusablity)
+//C# Features 1. Its Object Oriented (Security and Reusability) 2. Platfor Independent (That Means Cross Platform) 
+//3. Language Independent (That Means Cross Language Reusablity)
 //c#
 using System;
 namespace cSharpIntro
